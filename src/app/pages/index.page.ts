406ae3ba607fa2f94914598a8bec8056165e35d1
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-03-30 12:32:18 +0100

    overview page

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,46 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {injectContentFiles} from "@analogjs/content";
+import {RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
+import {FlyersAttributes} from "../../content/flyers/flyer-attributes.model";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  template: `
-    <div>
-      <a href="https://analogjs.org/" target="_blank">
-        <img alt="Analog Logo" class="logo analog" src="/analog.svg" />
-      </a>
-    </div>
-
-    <h2>Analog</h2>
-
-    <h3>The fullstack meta-framework for Angular!</h3>
-
-    <div class="card">
-      <button type="button" (click)="increment()">Count {{ count }}</button>
-    </div>
-
-    <p class="read-the-docs">
-      For guides on how to customize this project, visit the
-      <a href="https://analogjs.org" target="_blank">Analog documentation</a>
-    </p>
-  `,
-  styles: [
-    `
-      .logo {
-        will-change: filter;
+  selector: 'app-home', standalone: true, template: `
+    <ul>
+      <li><a href="about/">About</a></li>
+      @for (flyers of flyers;track flyers.attributes.slug) {
+        <a [routerLink]="['/flyers/', flyers.attributes.slug]">
+          <h2>{{ flyers.attributes.title }}</h2>
+        </a>
       }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .read-the-docs {
-        color: #888;
-      }
-    `,
-  ],
+    </ul>
+  `, imports: [RouterLink, NgForOf]
 })
 export default class HomeComponent {
-  count = 0;
-
-  increment() {
-    this.count++;
-  }
+  readonly flyers = injectContentFiles<FlyersAttributes>((contentFile) => contentFile.filename.includes('/src/content/flyers/'));
 }
